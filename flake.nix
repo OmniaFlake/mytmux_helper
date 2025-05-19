d152f1007c79a6223dfeda99ee3d0e7969fa8912
omnia <zzzooozzz836@gmail.com>
2025-05-19 13:55:25 +0545

    flake.lock

--- flake.nix
+++ flake.nix
@@ -1,21 +1,28 @@
 {
-  description = "My Rust package flake";
+  description = "My Rust package flake with overlay";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: 
-    flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs{inherit system;};
-  in
-  {
-    packages.myRustPackage = pkgs.myRustPackage;
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+      in {
+        packages.mytmux_helper = pkgs.mytmux_helper;
 
-    overlays.default  = final: prev:{
-      myRustPackages = prev.callPackage ./default.nix {};
-      };
-  });
+        overlays = {
+          default = self.overlay;
+        };
+
+        overlay = self.overlay;
+
+        # Other outputs like devShells, apps, etc.
+      });
 }
 
