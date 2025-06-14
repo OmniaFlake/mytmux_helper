97e52a518ec10128c13065c7a61f6ddbc676bf7c
omnia <zzzooozzz836@gmail.com>
2025-06-15 02:42:04 +0545

    changed arg into args where is_zoxide can pass more that one args where it was limited to single word now it's not bound to number of strings and accepts whole stringgg

---
+++ target/debug/deps/mytmux_helper-01fd36446c70bfac.d
@@ -0,0 +1,5 @@
+/home/omnia/mytmux_helper/target/debug/deps/mytmux_helper-01fd36446c70bfac: src/main.rs
+
+/home/omnia/mytmux_helper/target/debug/deps/mytmux_helper-01fd36446c70bfac.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/mytmux_helper.d
@@ -0,0 +1,1 @@
+/home/omnia/mytmux_helper/target/debug/mytmux_helper: /home/omnia/mytmux_helper/src/main.rs
